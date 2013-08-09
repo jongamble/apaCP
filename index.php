4b86ed9ec3e71b8b1c28554a5679b5dc1c4d53a1
Jon Gamble <jon@royalflushdesigns.net>
2013-08-09 17:01:56 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Apache Admin Panel</title>
+	<link rel="stylesheet" href="library/css/bootstrap.min.css">
+	<link rel="stylesheet" href="library/css/screen.css">
+
+    <script src="http://code.jquery.com/jquery.js"></script>
+    <script src="library/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header class="main-header">
+	<div class="container">
+		<h1 class="h1 col-12">Apache Admin Control Panel</h1>
+	</div>
+</header>
+<section class="container">
+	<article class="module create-new col-lg-5 col-sm-12">
+		<header class="module-header">
+			<h1 class="h2">Create a New Virtual Host</h1>
+		</header>
+		<section class="module-content">
+			<p>This is a test box to show the size and take up space.</p>
+		</section>
+	</article>
+	<article class="module view-active col-lg-push-2 col-lg-5 col-sm-12">
+		<header class="module-header">
+			<h1 class="h2">View Active Virtual Hosts</h1>
+		</header>
+		<section class="module-content">
+			<p>This is also a test box to show stuff.</p>
+		</section>
+	</article>
+</section>
+</body>
+</html>
